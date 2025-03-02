0d2dc1c856352a955f20d828040bc9cb1609660b
yoophoon <2420074212@qq.com>
2025-03-02 17:16:08 +0800

    https://nytimes.github.io/oak-byo-react-prosemirror-redux/post/build-your-own-pm-view/ Buld Your Own: ProseMirror View 一篇简易的介绍prosemirror-view的博客

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,243 @@
-import {EditorState, Plugin} from 'prosemirror-state'
-import { EditorView } from 'prosemirror-view'
+//@ts-nocheck
+import {EditorState, Plugin, TextSelection} from 'prosemirror-state'
 import { schema } from 'prosemirror-schema-basic'
+import { DirectEditorProps } from 'prosemirror-view'
+import { Node } from 'prosemirror-model'
 
-const counterPlugin=new Plugin({
-  state:{
-    init(){
-      return 0;
-    },
-    apply(tr, value, oldState, newState) {
-        const counterPluginMeta=tr.getMeta(counterPlugin)
-        switch(counterPluginMeta?.type){
-          case "counter/incremented":
-            return value+1;
-          case "counter/decremented":
-            return value-1;
-          default:
-            return value;
+//https://nytimes.github.io/oak-byo-react-prosemirror-redux/post/build-your-own-pm-view/
+//Buld Your Own: ProseMirror View
+
+function renderNode(node:Node){
+  if(node.isText){
+    return document.createTextNode(node.text??'')
+  }
+
+  const [tagName]=node.type.spec.toDOM(node)
+  return document.createElement(tagName)
+}
+
+
+class View{
+  constructor(public node,public readonly dom,public parent){
+    this.dom.__view=this
+  }
+
+  destory(){
+    this.parent=null
+    this.dom.__view=null
+  }
+
+  pointFromPos(pos, preferBefore) {
+    let index = 0;
+    let offset = 0;
+
+    while (index < this.children.length) {
+      const child = this.children[index];
+      const isLastChild = index === this.children.length - 1;
+
+      const { border, size } = child;
+      const start = offset + border;
+      const end = offset + size - border;
+      const after = end + border;
+
+      if (pos < after || (pos === after && preferBefore) || isLastChild) {
+        return child.pointFromPos(pos - start, preferBefore);
+      }
+
+      index = index + 1;
+      offset = offset + size;
+    }
+
+    return { node: this.dom, offset: pos };
+  }
+
+  get border() {
+    return 0;
+  }
+
+  get pos() {
+    const { parent } = this;
+
+    if (!parent) {
+      return -1;
+    }
+
+    const siblings = parent.children;
+    const index = siblings.indexOf(this);
+    const precedingSiblings = siblings.slice(0, index);
+    return precedingSiblings.reduce(
+      (pos, sibling) => pos + sibling.size,
+      parent.pos + parent.border
+    );
+  }
+
+  get size() {
+    return this.node.nodeSize;
+  }
+}
+
+class TextView extends View{
+  update(){
+    return false
+  }
+
+  pointFromPos(pos, preferBefore) {
+    return { node: this.dom, offset: pos };
+  }
+}
+
+class NodeView extends View{
+  constructor(node, dom, parent){
+    super(node, dom, parent)
+    this.children=[]
+    this.updateChildren()
+  }
+
+  get border() {
+    return this.node.isLeaf ? 0 : 1;
+  }
+
+  update(node) {
+    if (!this.node.sameMarkup(node)) {
+      return false;
+    }
+
+    this.node = node;
+    this.updateChildren();
+    return true;
+  }
+
+  updateChildren(){
+    this.node.forEach((child, offset, index) => {
+      const childView = this.children[index];
+      if (childView) {
+        const updated = childView.update(child);
+        if (updated) {
+          return;
         }
-    },
-  },
-  view:(view:EditorView)=>{
-    const countElement=document.querySelector('#count')!
-    const count=counterPlugin.getState(view.state)
-    countElement.innerHTML=count.toString()
+        if(childView.destroy)
+          childView.destroy();
+      }
 
-    document.querySelector('#increment')?.addEventListener('click',()=>{
-      const transaction=view.state.tr
-      transaction.setMeta(counterPlugin.key,{type:'counter/incremented'})
-      view.dispatch(transaction)
-    })
+      const childDOM = renderNode(child);
 
-    document.querySelector('#decrement')?.addEventListener('click',()=>{
-      const transaction=view.state.tr
-      transaction.setMeta(counterPlugin.key,{type:'counter/decremented'})
-      console.log(counterPlugin)
-      view.dispatch(transaction)
-    })
 
-    return {
-      update(view:EditorView,prevState:EditorState){
-        const count=counterPlugin.getState(view.state)
-        countElement.innerHTML=count.toString()
+      if (childView) {
+        this.dom.replaceChild(childDOM, childView.dom);
+      } else {
+        this.dom.appendChild(childDOM);
+      }
+
+
+      if (child.isText) {
+        this.children[index] = new TextView(child, childDOM, this);
+      } else {
+        this.children[index] = new NodeView(child, childDOM, this);
+      }
+    });
+
+    while (this.children.length > this.node.childCount) {
+      this.children.pop().destroy();
+      this.dom.removeChild(this.dom.lastChild);
+    }
+  }
+
+  destory(): void {
+      super.destory()
+      for(const child of children){
+        child.destory()
+      }
+  }
+}
+
+class EditorView extends NodeView{
+  state:EditorState
+  constructor(readonly dom:HTMLElement, readonly props:DirectEditorProps){
+    super(props.state.doc, dom, null)
+    this.state=props.state
+    this.dom.contentEditable="true"
+
+    this.onBeforeInput = this.onBeforeInput.bind(this);
+    this.dom.addEventListener("beforeinput", this.onBeforeInput);
+
+    this.onSelectionChange = this.onSelectionChange.bind(this);
+    document.addEventListener("selectionchange", this.onSelectionChange);
+  }
+
+  destory(){
+    super.destory()
+    this.dom.removeEventListener("beforeinput", this.onBeforeInput);
+    document.removeEventListener("selectionchange", this.onSelectionChange);
+  }
+
+  dispatch(tr) {
+    const newState = this.state.apply(tr);
+    this.setState(newState);
+  }
+
+  setState(newState) {
+    this.state = newState;
+    this.update(this.state.doc);
+  }
+
+  update(node) {
+    super.update(node);
+
+    const { anchor, head } = this.state.selection;
+    const backward = head > anchor;
+
+    const anchorPoint = this.pointFromPos(anchor, backward);
+    const focusPoint = this.pointFromPos(head, !backward);
+
+    const domSelection = document.getSelection();
+    domSelection.setBaseAndExtent(
+      anchorPoint.node,
+      anchorPoint.offset,
+      focusPoint.node,
+      focusPoint.offset
+    );
+  }
+
+  onBeforeInput(event) {
+    event.preventDefault();
+    switch (event.inputType) {
+      case "insertText": {
+        const { tr } = this.state;
+        tr.insertText(event.data);
+        this.dispatch(tr);
       }
     }
   }
-})
 
-// @ts-ignore
+  onSelectionChange(event) {
+    const { doc, tr } = this.state;
+
+    const domSelection = document.getSelection();
+
+    const { anchorNode, anchorOffset } = domSelection;
+    //FIXME select element out of eidtor,zhe anchorNode would be null
+    const anchorView = anchorNode.__view;
+    const anchor = anchorView.pos + anchorView.border + anchorOffset;
+    const $anchor = doc.resolve(anchor);
+
+    const { focusNode, focusOffset } = domSelection;
+    const headView = focusNode.__view;
+    const head = headView.pos + headView.border + focusOffset;
+    const $head = doc.resolve(head);
+
+    const selection = TextSelection.between($anchor, $head);
+    if (!this.state.selection.eq(selection)) {
+      tr.setSelection(selection);
+      this.dispatch(tr);
+    }
+  }
+}
+
+const doc = schema.node("doc", null, [
+  schema.node("paragraph", null, [schema.text("Hello, ProseMirror!")]),
+  schema.node("paragraph", null, [schema.text("Time to edit!")]),
+]);
+
 window.view=new EditorView(document.querySelector('#editor'),{
-  state:EditorState.create({
-    schema,
-    plugins:[counterPlugin]
-  })
+  state:EditorState.create({doc})
 })
-
