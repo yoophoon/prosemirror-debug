12572cd0b24b2d33fcb3983d9b68bef32d960975
yoophoon <2420074212@qq.com>
2025-03-25 14:40:25 +0800

    -

--- prosemirror/prosemirror-model/prosemirrorModelLearnLog.md
+++ prosemirror/prosemirror-model/prosemirrorModelLearnLog.md
@@ -6,3 +6,10 @@ doc：topNodeType，本质为一个node，当前文档在prosemirror的映射
 fragment：node的集合，parentNode和childrenNodes的连接层，方便对childrenNodes的一些操作,大多数对node子节点的操作都是经由fragment实现的
 node的结构：node.content=fragment,fragment.content=node[]
 > 类似的还有markSpec、mark、markType
+
+# isBlock,isInline,inlineContent
+节点类型的本质只有两种及内联节点和块节点，但包含inlineContent的block节点会被prosemirror内部认为是文本块节点，本质应该还是块节点
+
+
+# content.ts
+里面有一些NFA和DFA的东西，看不懂，等补完相关知识后再看
--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,44 @@
-import { EditorState, Plugin } from "prosemirror-state";
-import { EditorView } from "prosemirror-view";
+import { Node } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { EditorState } from "prosemirror-state";
+import { EditorView, NodeView } from "prosemirror-view";
 
-import './editor.css'
 
-let selectionSizePlugin=new Plugin({
-  view(editorView){return new SelectionSizeTooltip(editorView)}
-})
-
-class SelectionSizeTooltip{
-  tooltip:HTMLElement
-  constructor(view:EditorView){
-    this.tooltip=document.createElement('div')
-    this.tooltip.className='tooltip'
-    view.dom.parentNode!.appendChild(this.tooltip)
-    this.update(view,null)
+class ParagraphView implements NodeView {
+  dom:HTMLElement
+  contentDOM?: HTMLElement
+  constructor(node:Node) {
+    this.dom = document.createElement("div")
+    this.contentDOM = document.createElement("p")
+    
+    let label=document.createElement('label')
+    label.textContent='pragraph'
+    this.dom.appendChild(label)
+    this.dom.appendChild(this.contentDOM)
+    if (node.content.size == 0) this.dom.classList.add("empty")
   }
 
-  update(view:EditorView, lastState:EditorState|null){
-    let state=view.state
-    if(lastState&&lastState.doc.eq(state.doc)&&lastState.selection.eq(state.selection))
-      return
-
-    if(state.selection.empty){
-      this.tooltip.style.display='none'
-      return
+  update(node:Node) {
+    if (node.content.size > 0){
+      this.dom.classList.remove("empty")
+      // this.dom.textContent=node.toString()
     }
-
-    this.tooltip.style.display=''
-    let {from, to}=state.selection
-    let start=view.coordsAtPos(from),end=view.coordsAtPos(to)
-    let box=this.tooltip.offsetParent!.getBoundingClientRect()
-    let left=Math.max((start.left+end.left)/2,start.left+3)
-    this.tooltip.style.left=(left-box.left)+'px'
-    this.tooltip.style.bottom=(box.bottom-start.top)+'px'
-    this.tooltip.textContent=to-from+''
+    else{
+      this.dom.classList.add("empty")
+    }
+    return true
   }
-
-  destroy(){this.tooltip.remove()}
 }
 
-import { schema } from "prosemirror-schema-basic";
-// import { keymap  } from "prosemirror-keymap";
-// import { baseKeymap } from "prosemirror-commands";
-// import { history, undo, redo } from "prosemirror-history";
-// import { exampleSetup } from "prosemirror-example-setup";
-import { DOMParser } from "prosemirror-model";
-import './editor.css'
+let state=EditorState.create({
+  schema
+})
 
-// let state=EditorState.create({schema,plugins:[
-//   selectionSizePlugin,
-//   history(),
-//   keymap(baseKeymap),
-//   keymap({
-//     'Mod-z':undo,
-//     'Mod-y':redo,
-//   })
-// ]})
-//@ts-ignore
-window.view=new EditorView(document.querySelector('#editor'),{
-  state:EditorState.create({
-    doc:DOMParser.fromSchema(schema).parse(document.querySelector('#editor')!),
-    plugins:[
-      // ...exampleSetup({schema}),
-      selectionSizePlugin
-    ]
-  })
+let view=new EditorView(document.querySelector('#editor')!,{
+  state,
+  nodeViews:{
+    paragraph(node){
+      return new ParagraphView(node)
+    }
+  }
 })
